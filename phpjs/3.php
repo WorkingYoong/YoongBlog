e0f9c614ccb281590c06d8dc06fc80a6adc5d00b
WorkingYoong <Working.Yoong@gmail.com>
2016-04-27 16:40:52 +0900

    변수
    변수사용법

---
+++ phpjs/3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script charset="utf-8">
+        document .write("Hello World");
+    </script>
+    <?php
+      echo "Hello World";
+     ?>
+  </body>
+</html>
