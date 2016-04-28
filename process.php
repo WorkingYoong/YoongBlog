f9ad333e99a46d1f38a3d20d5aa6ceea530fbe6a
WorkingYoong <Working.Yoong@gmail.com>
2016-04-28 19:21:12 +0900

    라이브러리 만들고 갖다 쓰기까지 함

--- process.php
+++ process.php
@@ -1,22 +1,23 @@
 <?php
-  $conn = mysqli_connect("localhost","root","asdf16");
-  mysqli_select_db($conn,"YoongBlog");
+require("config/config.php");
+require("lib/db.php");
+$conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
 
-  $sql = "SELECT id FROM user where name = '".$_POST['author']."'";
-  $result = mysqli_query($conn, $sql);
-  if ($result->num_rows == 0) {
-    $sql = "INSERT INTO user (name,password) VALUES ('".$_POST['author']."','1111')";
-    mysqli_query($conn, $sql);
-    $user_id = mysqli_insert_id($conn);
-  }
-  else {
-    $row = mysqli_fetch_assoc($result);
+$title = mysqli_real_escape_string($conn, $_POST['title']);
+$author = mysqli_real_escape_string($conn, $_POST['author']);
+$description = mysqli_real_escape_string($conn, $_POST['description']);
 
-    $user_id = $row['id'];
-  }
-
-  $sql = "INSERT INTO topic (title,description,author,created) VALUES('".$_POST['title']."', '".$_POST['description']."', '".$user_id."', now())";
+$sql = "SELECT * FROM user WHERE name='".$author."'";
+$result  = mysqli_query($conn, $sql);
+if($result->num_rows == 0){
+  $sql = "INSERT INTO user (name, password) VALUES('".$author."', '111111')";
   mysqli_query($conn, $sql);
-
-  header('Location: http://localhost/YoongBlog/index.php');
- ?>
+  $user_id = mysqli_insert_id($conn);
+} else {
+  $row = mysqli_fetch_assoc($result);
+  $user_id = $row['id'];
+}
+$sql = "INSERT INTO topic (title,description,author,created) VALUES('".$title."', '".$description."', '".$user_id."', now())";
+$result = mysqli_query($conn, $sql);
+header('Location: http://localhost/YoongBlog/index.php');
+?>
--- write.php
+++ write.php
@@ -1,8 +1,9 @@
 <?php
-  $conn = mysqli_connect("localhost","root","asdf16");
-  mysqli_select_db($conn,"YoongBlog");
-  $result = mysqli_query($conn,"SELECT * FROM topic");
-   ?>
+require("config/config.php");
+require("lib/db.php");
+$conn = db_init($config["host"], $config["duser"], $config["dpw"], $config["dname"]);
+$result = mysqli_query($conn, "SELECT * FROM topic");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,12 +13,12 @@
 <body id="target">
     <header>
     <img src="https://s3-ap-northeast-1.amazonaws.com/opentutorialsfile/course/94.png" alt="생활코딩">
-        <h1><a href="http://localhost/YoongBlog/index.php">Yoong Blog</a></h1>
+        <h1><a href="http://localhost/YoongBlog/index.php">JavaScript</a></h1>
   </header>
     <nav>
         <ol>
     <?php
-    while ($row = mysqli_fetch_assoc($result)){
+    while( $row = mysqli_fetch_assoc($result)){
       echo '<li><a href="http://localhost/YoongBlog/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
     }
     ?>
@@ -29,18 +30,17 @@
     <a href="http://localhost/YoongBlog/write.php">쓰기</a>
   </div>
   <article>
-    <form class="" action="process.php" method="post">
+    <form action="process.php" method="post">
       <p>
-        제목 : <input type="text" name="title" value="">
+        제목 : <input type="text" name="title">
       </p>
       <p>
-        작성자 : <input type="text" name="author" value="">
+        작성자 : <input type="text" name="author">
       </p>
       <p>
         본문 : <textarea name="description"></textarea>
       </p>
-      <input type="submit" name="name" >
-
+      <input type="submit" name="name">
     </form>
   </article>
 </body>
