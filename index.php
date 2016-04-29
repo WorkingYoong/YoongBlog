0b59b74a9480aa94541f3904566a41cd4feba38f
WorkingYoong <Working.Yoong@gmail.com>
2016-04-29 14:59:03 +0900

    부트스트랩 디자인 적용

--- index.php
+++ index.php
@@ -7,39 +7,54 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
 <!DOCTYPE html>
 <html>
 <head>
-     <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="http://localhost/YoongBlog/css/style.css">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<link rel="stylesheet" type="text/css" href="http://localhost/YoongBlog/css/style.css">
+<link href="http://localhost/YoongBlog\bootstrap-3.3.6-dist\css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body id="target">
-    <header>
-    <img src="https://s3-ap-northeast-1.amazonaws.com/opentutorialsfile/course/94.png" alt="생활코딩">
-        <h1><a href="http://localhost/YoongBlog/index.php">JavaScript</a></h1>
-  </header>
-    <nav>
-        <ol>
-    <?php
-    while( $row = mysqli_fetch_assoc($result)){
-      echo '<li><a href="http://localhost/YoongBlog/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
-    }
-    ?>
-        </ ol>
-    </nav>
-  <div id="control">
-    <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
-    <input type="button" value="black" onclick="document.getElementById('target').className='black'" />
-    <a href="http://localhost/YoongBlog/write.php">쓰기</a>
+  <div class="container-fluid">
+
+    <header class="jumbotron text-center">
+      <h1><a href="http://localhost/YoongBlog/index.php">YoongBlog</a></h1>
+    </header>
+    <div class="row">
+        <nav class="col-md-2">
+          <ol class="nav nav-pills nav-stacked" >
+            <?php
+            while( $row = mysqli_fetch_assoc($result)){
+              echo '<li><a href="http://localhost/YoongBlog/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+            }
+            ?>
+          </ol>
+        </nav>
+      <div class="col-md-9">
+        <article>
+          <?php
+          if(empty($_GET['id']) === false ) {
+            $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_assoc($result);
+            echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+            echo '<p>'.htmlspecialchars($row['name']).'</p>';
+            echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
+          }
+          ?>
+        </article>
+        <hr/>
+        <div id="control">
+          <input type="button" value="black" onclick="document.getElementById('target').className='black'" class="btn btn-default btn-lg"/>
+          <input type="button" value="white" onclick="document.getElementById('target').className='white'" class="btn btn-default btn-lg"/>
+          <a href="http://localhost/YoongBlog/write.php" class="btn btn-success btn-lg">쓰기</a>
+        </div>
+      </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://localhost/YoongBlog\bootstrap-3.3.6-dist\js/bootstrap.min.js"></script>
   </div>
-  <article>
-  <?php
-  if(empty($_GET['id']) === false ) {
-      $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
-      $result = mysqli_query($conn, $sql);
-      $row = mysqli_fetch_assoc($result);
-      echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
-      echo '<p>'.htmlspecialchars($row['name']).'</p>';
-      echo strip_tags($row['description'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
-  }
-  ?>
-  </article>
 </body>
 </html>
--- write.php
+++ write.php
@@ -7,41 +7,59 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
 <!DOCTYPE html>
 <html>
 <head>
-     <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="http://localhost/YoongBlog/css/style.css">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<link rel="stylesheet" type="text/css" href="http://localhost/YoongBlog/css/style.css">
+<link href="http://localhost/YoongBlog\bootstrap-3.3.6-dist\css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body id="target">
-    <header>
-    <img src="https://s3-ap-northeast-1.amazonaws.com/opentutorialsfile/course/94.png" alt="생활코딩">
-        <h1><a href="http://localhost/YoongBlog/index.php">JavaScript</a></h1>
-  </header>
-    <nav>
-        <ol>
-    <?php
-    while( $row = mysqli_fetch_assoc($result)){
-      echo '<li><a href="http://localhost/YoongBlog/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
-    }
-    ?>
-        </ ol>
-    </nav>
-  <div id="control">
-    <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
-    <input type="button" value="black" onclick="document.getElementById('target').className='black'" />
-    <a href="http://localhost/YoongBlog/write.php">쓰기</a>
+  <div class="container-fluid">
+    <header class="jumbotron text-center">
+      <h1><a href="http://localhost/YoongBlog/index.php">YoongBlog</a></h1>
+    </header>
+    <div class="row">
+        <nav class="col-md-2">
+          <ol class="nav nav-pills nav-stacked" >
+            <?php
+            while( $row = mysqli_fetch_assoc($result)){
+              echo '<li><a href="http://localhost/YoongBlog/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
+            }
+            ?>
+          </ol>
+        </nav>
+      <div class="col-md-9">
+        <article>
+
+          <form action="process.php" method="post">
+              <label for="form-title">제목</label>
+              <div class="form-group">
+              <input type="text" class="form-control" name="title" id="form-title" placeholder="제목">
+            </div>
+            <div class="form-group">
+              <label for="form-author">작성자</label>
+              <input type="text" class="form-control" name="author" id="form-author" placeholder="작성자">
+            </div>
+            <div class="form-group">
+              <label for="form-description">본문</label>
+              <textarea class="form-control" row="10" name="description" id="form-description" placeholder="본문을 적어주세요."></textarea>
+            </div>
+              <input type="submit" name="name" class="btn btn-success">
+          </form>
+        </article>
+        <hr/>
+        <div id="control">
+          <input type="button" value="black" onclick="document.getElementById('target').className='black'" class="btn btn-default btn-lg"/>
+          <input type="button" value="white" onclick="document.getElementById('target').className='white'" class="btn btn-default btn-lg"/>
+          <a href="http://localhost/YoongBlog/write.php" class="btn btn-success btn-lg">쓰기</a>
+        </div>
+      </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://localhost/YoongBlog\bootstrap-3.3.6-dist\js/bootstrap.min.js"></script>
   </div>
-  <article>
-    <form action="process.php" method="post">
-      <p>
-        제목 : <input type="text" name="title">
-      </p>
-      <p>
-        작성자 : <input type="text" name="author">
-      </p>
-      <p>
-        본문 : <textarea name="description"></textarea>
-      </p>
-      <input type="submit" name="name">
-    </form>
-  </article>
 </body>
 </html>
